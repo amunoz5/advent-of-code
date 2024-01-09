b1c88a041be5e3f85cf8704da5fe1c0d7887a354
Aída Muñoz Monjas <amunoza225@gmail.com>
2024-01-09 19:40:39 +0100

    2023 day 4 - short version

---
+++ dia4_small.py
@@ -0,0 +1,18 @@
+import re
+
+with open("data/input4.txt") as f:
+    data = [[1] + re.sub('Card \d: ', '', line[:-1]).split('|') for line in f]
+    
+star1 = 0
+for i in range(len(data)):
+    match = 0
+    for num in data[i][2].strip().split():
+        match += 1 if num in data[i][1].strip().split() else 0
+    star1 += 2**(match-1) if match > 0 else 0
+
+    for _ in range(data[i][0]):
+        for j in [x+1 for x in range(match)]:
+            data[i+j][0] += 1
+    
+print("first star:", star1)
+print("second star:", sum([data[i][0] for i in range(len(data))]))
